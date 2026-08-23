f859043567452935bb06b636a3437ac52db4cbf6
itsasmaulhosna <itsasmaulhosna@gmail.com>
2026-08-23 23:41:53 +0600

    String created

---
+++ string.js
@@ -0,0 +1,11 @@
+const firstName = 'John';
+const lastName = 'Doe';
+// const fullName = firstName + ' ' + lastName;
+const fullName = `${firstName} ${lastName}\nNoagaon`;
+console.log(fullName);
+
+let randomString = 'Hello, World!';
+console.log(randomString.length);
+console.log(randomString.toUpperCase());
+console.log(randomString.toLowerCase());
+console.log(randomString.includes('World'));
