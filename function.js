2d301e2833334202104c779323752fbfc20f06c3
itsasmaulhosna <itsasmaulhosna@gmail.com>
2026-08-24 18:59:30 +0600

    Arraw function

--- function.js
+++ function.js
@@ -12,3 +12,18 @@ function greet(name) {
 }
 const greeting = greet('Alice');
 console.log(greeting);
+
+// arrow function
+const total2 = (price, quantity = 12) => {
+  const totalPrice = price * quantity;
+  return totalPrice;
+};
+
+const spend2 = total2(10);
+const save2 = 200 - spend2;
+console.log(spend2);
+console.log(save2);
+
+const add = (x, y) => x + y;
+const sum = add(5, 3);
+console.log(sum);
