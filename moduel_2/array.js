246a83797e16dea8ce14f0a04e3be4ea4a1ce2e1
itsasmaulhosna <itsasmaulhosna@gmail.com>
2026-08-24 23:44:32 +0600

    Implemented push,pos,shift,splice

--- moduel_2/array.js
+++ moduel_2/array.js
@@ -1,2 +1,14 @@
-let name = ['Maya', 'Mina', 'Naema', 'Nisa'];
-console.log(name[3]);
+let name = ['Maya', 'Mina', 'Naema', 'Nisa', 'Zain', 'Nirob'];
+let number = [1, 2, 3, 4, 5];
+console.log(name[3], number[3]);
+
+name.push('Rita');
+number.pop(5);
+// console.log(name, number);
+
+name.unshift('Zain');
+name.shift();
+console.log(name);
+
+name.splice(2, 1, 'Mithila');
+console.log(name);
