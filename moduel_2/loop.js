dbe284a12dcf3884cf26cdcd89d99e93c19de9f8
itsasmaulhosna <itsasmaulhosna@gmail.com>
2026-08-26 10:50:43 +0600

    Loop

---
+++ moduel_2/loop.js
@@ -0,0 +1,20 @@
+// for
+for (let i = 0; i < 5; i++) {
+  console.log(`count: ${i}`);
+}
+
+for (let i = 4; i >= 0; i--) {
+  console.log(`count: ${i}`);
+}
+
+let fruits = ['Mango', 'Aam', 'Lichu', 'Kola'];
+for (let i = 0; i < fruits.length; i++) {
+  console.log('Khabo:', fruits[i]);
+}
+
+// while
+let i = 0;
+while (i < fruits.length) {
+  console.log('Khabo:', fruits[i]);
+  i++;
+}
