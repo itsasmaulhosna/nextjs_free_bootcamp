021810bffbe3b26199c05d98759613347362c943
itsasmaulhosna <itsasmaulhosna@gmail.com>
2026-08-31 23:20:14 +0600

    find ,include,some,,every implemented

--- moduel_3/arrOps.js
+++ moduel_3/arrOps.js
@@ -1,4 +1,4 @@
-let fruits = ['Mango', 'Banana', 'Lichi', 'Annar'];
+let fruits = ['Mango', 'Banana', 'Lichi', 'Annar', 'Jackfruite'];
 let newFruits = fruits.forEach((f, indx) => {
   console.log(`${indx} --> ${f}`);
 });
@@ -9,3 +9,24 @@ console.log(newFruits2, '\n', fruits);
 // filter
 let custom = fruits.filter((f) => f.length > 5);
 console.log(custom);
+// find>1st match
+let custom2 = fruits.find((f) => f.length > 5);
+console.log(custom2);
+
+// include>check item
+
+let findFr = fruits.includes('Mangos');
+console.log(findFr);
+
+// some
+const student = [
+  { name: 'Maya', marks: 45 },
+  { name: 'Taya', marks: 75 },
+  { name: 'Jaya', marks: 95 },
+  { name: 'Naya', marks: 35 },
+];
+let studentCheck = student.some((s) => s.marks > 80);
+console.log(studentCheck);
+
+let studentCheck2 = student.every((s) => s.marks > 80);
+console.log(studentCheck2);
